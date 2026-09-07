d305fcdee9bf779a9c03993b6d625e494f045f35
Matthew Wicklund <mattwicklundlessons@gmail.com>
2026-09-07 14:31:09 +0300

    Initial React TypeScript Vite project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <h1>Matthew Wicklund</h1>
+      <p>Software Engineer · Music Technology · Recording Artist</p>
+    </main>
+  )
+}
+
+export default App
