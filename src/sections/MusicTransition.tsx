04fe2372391df17ef76e700349b8f00290d945af
Matthew Wicklund <mattwicklundlessons@gmail.com>
2026-09-07 17:28:33 +0300

    Fix live performance image path

--- src/sections/MusicTransition.tsx
+++ src/sections/MusicTransition.tsx
@@ -6,7 +6,7 @@ function MusicTransition() {
       <Container>
         <div className="music-transition__image-wrap">
           <img
-            src="public/images/GSOMattGermany1.jpg"
+            src="/images/GSOMattGermany1.jpg"
             alt="Matthew Wicklund performing live on guitar"
             className="music-transition__image"
           />
